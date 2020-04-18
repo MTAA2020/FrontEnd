f73447d0b28e8fa73e7028f14d7652db05b8fc68
Gergely Lengyel <32176039+gergely73@users.noreply.github.com>
2020-04-18 21:20:22 +0200

    update

--- Appka/frontik/lib/user/category.dart
+++ Appka/frontik/lib/user/category.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class MyCategory extends StatefulWidget {
   MyCategory({Key key, this.title}) : super(key: key);
@@ -11,16 +11,112 @@ class MyCategory extends StatefulWidget {
 }
 
 class _MyCategoryState extends State<MyCategory> {
+
+
+
+
+    Widget stars() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: RatingBar(
+        initialRating: 0,
+        minRating: 1,
+        direction: Axis.horizontal,
+        allowHalfRating: true,
+        itemCount: 5,
+        itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+        itemBuilder: (context, _) => Icon(
+          Icons.star,
+          color: Colors.amber,
+        ),
+        onRatingUpdate: (rating) {
+          print(rating);
+        },
+      )
+    );
+  }
+
+  Widget image(obrazok){
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Image.network(
+        obrazok,
+        height: 167.0,
+        width: 113.0,
+      	),
+    );
+  }
+
+  Container kniha(String title, String obrazok,String author){
+    return Container(
+      padding: EdgeInsets.fromLTRB(10,5,10,0),
+      height: 180,
+      width: double.maxFinite,
+      child: Card(
+        elevation: 5,
+        child: Padding(
+        padding: EdgeInsets.all(0),
+        child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerRight,
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(left: 0, top: 0),
+                    child: Row(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            image(obrazok)
+                          ],
+                        ),
+                      stars()
+                      ],
+                    )
+                ),
+              ],
+            ),
+          )
+        ]),
+        ),
+      ),
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
-    
+
+
+    Widget picksforyouscroll = new Container(
+    margin: EdgeInsets.symmetric(vertical:1.0,horizontal: 5.0),
+    height: 500.0,
+    child: new ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        kniha("Toto je kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Harry Potter a Kamen mudrcov", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Tretie kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Najlepsia kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1")
+      ],
+    ));
+
+
     return new Scaffold(
       backgroundColor: Colors.grey,
       appBar: new AppBar(
         title: new Text(widget.title),
         centerTitle: true,
       ),
+      body: new Center(
+        child: new Container(
+          child: ListView(
+            children: <Widget>[
+              picksforyouscroll,
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- Appka/frontik/lib/user/mybooks.dart
+++ Appka/frontik/lib/user/mybooks.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:frontik/user/beforelogin.dart';
 
 class MyBooks extends StatefulWidget {
   MyBooks({Key key, this.title}) : super(key: key);
@@ -12,34 +14,70 @@ class MyBooks extends StatefulWidget {
 class _MyBooksState extends State<MyBooks> {
 
 
-  Container kniha(String nazov, String obrazok){
-    return Container(
-      width: 120.0,
-      height: 200.0,
-      child: Card(
-        color: Colors.grey,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(20),
-          top: Radius.circular(20)
-          ),
+  Widget stars() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: RatingBar(
+        initialRating: 0,
+        minRating: 1,
+        direction: Axis.horizontal,
+        allowHalfRating: true,
+        itemCount: 5,
+        itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+        itemBuilder: (context, _) => Icon(
+          Icons.star,
+          color: Colors.amber,
         ),
-        child: new Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-              child: Image.network(
-                  obrazok,
-                  height: 160.0,
-                  width: 140.0,
-              ),
+        onRatingUpdate: (rating) {
+          print(rating);
+        },
+      )
+    );
+  }
+
+  Widget image(obrazok){
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Image.network(
+        obrazok,
+        height: 167.0,
+        width: 113.0,
+      	),
+    );
+  }
+
+  Container kniha(String title, String obrazok,String author){
+    return Container(
+      padding: EdgeInsets.fromLTRB(10,5,10,0),
+      height: 180,
+      width: double.maxFinite,
+      child: Card(
+        elevation: 5,
+        child: Padding(
+        padding: EdgeInsets.all(0),
+        child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerRight,
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(left: 0, top: 0),
+                    child: Row(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            image(obrazok)
+                          ],
+                        ),
+                      stars()
+                      ],
+                    )
+                ),
+              ],
             ),
-            new Text(
-                        nazov,
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0 ),
-                        textAlign: TextAlign.center,
-                      )
-          ],
+          )
+        ]),
         ),
       ),
     );
@@ -48,9 +86,32 @@ class _MyBooksState extends State<MyBooks> {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget picksforyouscroll = new Container(
+    margin: EdgeInsets.symmetric(vertical:1.0,horizontal: 5.0),
+    height: 500.0,
+    child: new ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        kniha("Toto je kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Harry Potter a Kamen mudrcov", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Tretie kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1"),
+        kniha("Najlepsia kniha", "https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","author1")
+      ],
+    ));
+
     return new Scaffold(
-      backgroundColor: Colors.grey
+      backgroundColor: Colors.grey,
       
+      body: new Center(
+        child: new Container(
+          child: ListView(
+            children: <Widget>[
+              picksforyouscroll,
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
