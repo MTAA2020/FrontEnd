1a10db19c7fca74b4a940c021b5a00fb6e1c4e6f
itchybunion6235 <adamko.zilla@gmail.com>
2020-04-21 10:49:56 +0200

    Icons admin main

--- Appka/frontik/lib/admin/adminMain.dart
+++ Appka/frontik/lib/admin/adminMain.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new AwesomeButton()
-  ));
+  runApp(new MaterialApp(home: new AwesomeButton()));
 }
 
 class AwesomeButton extends StatefulWidget {
@@ -12,42 +10,84 @@ class AwesomeButton extends StatefulWidget {
 }
 
 class AwesomeButtonState extends State<AwesomeButton> {
-
-  int counter = 0;
-  List<String> strings = ["Flutter", "Is", "Awesome"];
-  String displayedString = "";
-
-  //you can also declare variables like this
-  // var counter = 0;
-  // var strings = ["Flutter", "Is", "Awesome"];
-
-  void onPressed(){
-    setState(() {
-      displayedString = strings[counter];
-      counter = counter < 2 ? counter+1 : 0;
-    });
+  void onPressed() {
+    setState(() {});
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Stateful Widget!"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+        appBar: new AppBar(
+          automaticallyImplyLeading: false,
+          title: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              text: 'Admin Read It!',
+              style: TextStyle(fontFamily: 'EmilysCandy', fontSize: 30),
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AwesomeButton()),
+                  );
+                }),
+            IconButton(
+                icon: Icon(Icons.exit_to_app),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AwesomeButton()),
+                  );
+                })
+          ],
+          centerTitle: true,
+        ),
+        body: new Container(
+            child: new Center(
+                child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
               new Padding(padding: new EdgeInsets.all(15.0)),
               new RaisedButton(
-                child: new Text("Press me!", style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20.0)),
-                color: Colors.red,
-                onPressed: onPressed
-              )
-            ]
-          )
-        )
-      )
-    );
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(60), top: Radius.circular(60)),
+                  ),
+                  child: new Text("Press me!",
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0)),
+                  color: Colors.red,
+                  onPressed: onPressed),
+              new RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(60), top: Radius.circular(60)),
+                  ),
+                  child: new Text("Press me 2!",
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0)),
+                  color: Colors.red,
+                  onPressed: onPressed),
+              new RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(60), top: Radius.circular(60)),
+                  ),
+                  child: new Text("Press me 3!",
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0)),
+                  color: Colors.red,
+                  onPressed: onPressed)
+            ]))));
   }
 }
