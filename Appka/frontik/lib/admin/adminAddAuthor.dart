fd511e0bca9db119af12a8f71aa278d0836f739f
itchybunion6235 <adamko.zilla@gmail.com>
2020-04-23 22:56:24 +0200

    zmeny add author

--- Appka/frontik/lib/admin/adminAddAuthor.dart
+++ Appka/frontik/lib/admin/adminAddAuthor.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(new MaterialApp(home: new AddAuthor()));
@@ -9,7 +12,26 @@ class AddAuthor extends StatefulWidget {
   AddAuthorState createState() => new AddAuthorState();
 }
 
+class Author {
+  final String name;
+  final String about;
+  
+
+  Author({this.name,this.about});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+
+    return new Author(
+      name: json['name'],
+      about: json['about']
+    );
+  }
+
+}
+
 class AddAuthorState extends State<AddAuthor> {
+  var authorName = TextEditingController();
+  var about = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -33,7 +55,7 @@ class AddAuthorState extends State<AddAuthor> {
                 child: new Text('Enter Author details',
                     style: TextStyle(fontFamily: 'EmilysCandy', fontSize: 30)),
               ),
-              Container(
+              /*Container(
                 width: 300,
                 padding: EdgeInsets.all(10),
                 child: new TextField(
@@ -42,15 +64,16 @@ class AddAuthorState extends State<AddAuthor> {
                       border: OutlineInputBorder(),
                       hintText: "First Name"),
                 ),
-              ),
+              ),*/
               Container(
                 width: 300,
                 padding: EdgeInsets.all(10),
                 child: new TextField(
+                  controller: authorName,
                   decoration: new InputDecoration(
                       fillColor: Colors.white,
                       border: OutlineInputBorder(),
-                      hintText: "Last Name"),
+                      hintText: "Author's Name"),
                 ),
               ),
               Container(
@@ -58,6 +81,7 @@ class AddAuthorState extends State<AddAuthor> {
                 width: 300,
                 padding: EdgeInsets.all(10),
                 child: new TextField(
+                  controller: about,
                   decoration: new InputDecoration(
                       fillColor: Colors.white,
                       border: OutlineInputBorder(),
@@ -71,26 +95,56 @@ class AddAuthorState extends State<AddAuthor> {
                   minWidth: 200,
                   height: 60,
                   child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.vertical(
-                            bottom: Radius.circular(15),
-                            top: Radius.circular(15)),
-                      ),
-                      child: new Text("Submit",
-                          style: new TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'EmilysCandy',
-                              fontSize: 20.0)),
-                      color: Colors.lightBlue,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AddAuthor()),
-                        );
-                      }),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.circular(15),
+                          top: Radius.circular(15)),
+                    ),
+                    child: new Text("Submit",
+                        style: new TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'EmilysCandy',
+                            fontSize: 20.0)),
+                    color: Colors.lightBlue,
+                    onPressed: (){give10();},
+                  ),
                 ),
               ),
               //displaying input text
             ]))));
   }
+    Future give10() async {
+    var url = Uri.http('10.0.2.2:5000', "/addAuthor");
+    var body = jsonEncode({'name':'${authorName.text}',
+    'about':about.text});
+    http.Response response;
+    try{
+      response = await http.post(
+        url,
+        headers: {
+          'Content-Type' : 'application/json',
+          'Connection' : 'keep-alive'
+        },
+        body: body
+      );
+    }
+    catch(error){
+      print(error);
+
+    }
+
+    final jsonResponse = json.decode(response.body);
+    Author autor = Author.fromJson(jsonResponse);
+    
+    if (response.statusCode==200){
+      setState(() {
+          print(autor.name);
+      });
+    }
+    else{
+      throw Exception('fail');
+    }
+    
+  }
+
 }
